ac73ea82845aee496763becce0a4aa97a7c857a3
haiyanx <haiyanxuu@gmail.com>
2019-02-28 17:06:51 -0500

    updated settings

--- mitocgallery/settings.py
+++ mitocgallery/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'imagestore',
+    'sorl.thumbnail',
+    'tagging',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'mitocgallery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+#MEDIA_ROOT = '/imagestore/static/imagestore.css'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = '/media/'
--- mitocgallery/urls.py
+++ mitocgallery/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^gallery/', include(('imagestore.urls', 'imagestore'), namespace='imagestore')),
+    #url(r'^$', views.home, name='home'),
+    url(r'^$', TemplateView.as_view(template_name='main.html')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

c3525cdc36e336bebeaefd58b67cd04aed028e4f
haiyanx <haiyanxuu@gmail.com>
2019-03-12 15:57:15 -0400

    Add accounts app (signup, login, log out). Add bootstrap.

--- mitocgallery/urls.py
+++ mitocgallery/urls.py
@@ -19,10 +19,11 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import TemplateView
+import accounts.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^gallery/', include(('imagestore.urls', 'imagestore'), namespace='imagestore')),
-    #url(r'^$', views.home, name='home'),
-    url(r'^$', TemplateView.as_view(template_name='main.html')),
+    url(r'^accounts/', include('accounts.urls')),
+    url(r'^$', TemplateView.as_view(template_name='main.html'), name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

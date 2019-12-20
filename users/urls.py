ee33e134dbbee348fd61ec07b4a65902bc724e0e
haiyanx <haiyanxuu@gmail.com>
2019-12-20 17:58:03 -0500

    Delete unused app users

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-# users/urls.py
-#
-# from django.urls import path, include
-# from . import views
-# from .views import SignUpView
-
-# urlpatterns = [
-#     path('signup/', SignUpView.as_view(), name='signup'),
-# ]

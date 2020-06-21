9c97e5a03041534eb1cf8a56088735a49c40e6bf
Haiyan Xu <haiyanxuu@gmail.com>
2020-06-21 15:51:21 -0400

    Update production settings with email config

--- mitocgallery/production_settings.py
+++ mitocgallery/production_settings.py
@@ -183,4 +183,9 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND_VAR')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
